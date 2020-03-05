8f837a4f8c5fdd72dd319b9610e80e6bfc709a90
Nicholas Garon <garonnj@dukes.jmu.edu>
2020-03-04 19:32:59 -0500

    Add project files.

---
+++ ElkTrucking/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElkTrucking.Startup))]
+namespace ElkTrucking
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
